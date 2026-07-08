25c3747e9fe8ab1d0a3eafddfe586b5374dfec69
razanalk <13238holbeertonschool@gmail.com>
2026-07-08 14:41:13 +0200

    Set up project structure for Part 2

---
+++ part2/README.md
@@ -0,0 +1,16 @@
+# HBnB Project
+
+This project implements the Business Logic and API layers of the HBnB application.
+
+## Project Structure
+
+- app/api: API endpoints
+- app/models: Business logic models
+- app/services: Facade layer
+- app/persistence: In-memory repository
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+python run.py
---
+++ part2/app/services/facade.py
@@ -0,0 +1,17 @@
+from app.persistence.repository import InMemoryRepository
+
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        # Implementation will be added in future tasks
+        pass
+
+    def get_place(self, place_id):
+        # Implementation will be added in future tasks
+        pass
