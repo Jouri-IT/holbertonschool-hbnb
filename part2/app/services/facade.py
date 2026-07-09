d5fd95055c8ec51b91ccc5b812513024cfc66a88
rywma <almujallireema@gmail.com>
2026-07-09 17:37:48 +0300

    Implement Business Logic Layer

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 
 class HBnBFacade:
@@ -9,9 +10,23 @@ class HBnBFacade:
         self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
-        # Implementation will be added in future tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+    
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        user.update(user_data)
+        return user
+    
     def get_place(self, place_id):
         # Implementation will be added in future tasks
         pass
