754893c19909649ef38d97d21e274917c7f0dac1
Jouri <jouri.f.alsulaiman@gmail.com>
2026-07-13 00:23:00 +0300

    Implement amenity operations in HBnBFacade
    Added methods for amenity operations including creation, retrieval, and updating of amenities.

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity  # Import the Amenity model
 
 
 class HBnBFacade:
@@ -9,6 +10,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    # --- User Operations ---
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -31,6 +33,30 @@ class HBnBFacade:
         user.update(user_data)
         return user
 
+    # --- Amenity Operations ---
+    def create_amenity(self, amenity_data):
+        """Instantiates and registers a new amenity into memory."""
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        """Fetches a specific amenity by its UUID."""
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """Returns all registered amenities. Needed for GET /api/v1/amenities/."""
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """Updates attributes of an existing amenity."""
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        amenity.update(amenity_data)
+        return amenity
+
+    # --- Place Operations (Placeholders) ---
     def get_place(self, place_id):
         # Implementation will be added in future tasks
         pass
