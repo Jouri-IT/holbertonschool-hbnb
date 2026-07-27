909adcd83fe2985b107b084b4a487b92032f64cf
razanalk <13238holbeertonschool@gmail.com>
2026-07-27 16:25:10 +0200

    Implement JWT authentication

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -10,13 +10,14 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Use the existing SECRET_KEY for JWT
+    # Use Flask SECRET_KEY for JWT
     app.config["JWT_SECRET_KEY"] = app.config["SECRET_KEY"]
 
     bcrypt.init_app(app)
@@ -34,5 +35,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(auth_ns, path="/api/v1/auth")
 
     return app
