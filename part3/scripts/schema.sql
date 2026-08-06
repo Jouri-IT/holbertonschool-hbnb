fb7e1312c2aeb54191d6a9dea0346ab704a44295
rywma <almujallireema@gmail.com>
2026-08-06 23:18:55 +0300

    Task 9: Editing sql scripts

--- part3/scripts/schema.sql
+++ part3/scripts/schema.sql
@@ -8,48 +8,48 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(120) NOT NULL UNIQUE,
     password VARCHAR(128) NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT 0,
-    created_at DATETIME,
-    updated_at DATETIME
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS places (
-    id CHAR(36) PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
     description VARCHAR(500),
     price FLOAT NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
     owner_id CHAR(36) NOT NULL,
-    created_at DATETIME,
-    updated_at DATETIME,
-    FOREIGN KEY (ownder_id) REFERENCES user(id)
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
-    id CHAR(36) PRIMARY KEY,
-    text VARCHAR(1000)  NOT NULL,
-    rating INTEGER NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    text VARCHAR(1000) NOT NULL,
+    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
     user_id CHAR(36) NOT NULL,
-    place_id CHAR(36) NOT NULL,
-    created_at DATETIME,
-    updated_at DATETIME,
+    place_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (place_id) REFERENCES places(id),
     UNIQUE (user_id, place_id)
 );
 
 CREATE TABLE IF NOT EXISTS amenities (
-    id CHAR(36) PRIMARY KEY,
-    name VARCHAR(50)) NOT NULL UNIQUE,
-    description VARCHAR(225),
-    created_at DATETIME,
-    updated_at DATETIME
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS place_amenity (
-    place_id CHAR(36) NOT NULL,
-    amenity_id CHAR(36) NOT NULL,
-    PRIMARY KEY (place_id), (amenity_id),
-    FOREIGN KEY (place_id) REFERENCES place(sid),
+    place_id INT NOT NULL,
+    amenity_id INT NOT NULL,
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
     FOREIGN KEY (amenity_id) REFERENCES amenities(id)
 );
--- part3/scripts/seed.sql
+++ part3/scripts/seed.sql
@@ -1,17 +1,7 @@
-INSERT INTO users (id, first_name, last_name, email, password, is_admin, created_at, updated_at)
-VALUES (
-    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
-    'Admin',
-    'HBnB',
-    'admin@hbnb.io',
-    '$2b$12$omgd5ArmtQieR0a034cxdePjrdTzdIFpsRulfZdRGuEaTdQFm/v3u',
-    1,
-    datetime('now'),
-    datetime('now')
-);
+USE hbnb_db;
 
-INSERT INTO amenities (name, description, created_at, updated_at)
-VALUES
-    ('Wifi', '', datetime('now'), datetime('now')),
-    ('Swimming Pool', '' datetime('now'), datetime('now')),
-    ('Air Conditioning', '', datetime('now'), datetime('now'));
+REPLACE INTO users (id, first_name, last_name, email, password, is_admin) 
+VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1', 'Admin', 'HBnB', 'admin@hbnb.io', '$2b$12$omgd5ArmtQieR0a034cxdePjrdTzdIFpsRulfZdRGuEaTdQFm/v3u', 1);
+
+REPLACE INTO amenities (id, name, description) 
+VALUES (1, 'WiFi', ''), (2, 'Swimming Pool', ''), (3, 'Air Conditioning', '');
